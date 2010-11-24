09b4c1dca1b6798678719c2b5753f2bbfbcb30eb
bramveen <bramveenhof@gmail.com>
2010-11-24 20:08:12 +0100

    Added icons

---
+++ GReader/GReader/Settings.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+
+namespace GReader
+{
+    public partial class Settings : PhoneApplicationPage
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+            settings["username"] = txtUsername.Text;
+            settings["password"] = txtPassword.Password;
+            settings.Save();
+
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
